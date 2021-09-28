8b7b080b29b0c00b7bbd37a5c2d85430dae07882
Resteu Mark <91153508+resteumark24@users.noreply.github.com>
2021-09-28 21:49:22 +0300

    Update 1.Sa se afiseze cel mai mare numar.py

--- 1.Sa se afiseze cel mai mare numar.py
+++ 1.Sa se afiseze cel mai mare numar.py
@@ -1,11 +1,11 @@
 #[METODA I]#
-l = [i for i in range(10)]
-l.sort()
-print("Cel mai mare numar este:", l[-1])
+#l = [i for i in range(10)]
+#l.sort()
+#print("Cel mai mare numar este:", l[-1])
 
 #[METODA II]
-l = [i for i in range(10)]
-print("Cel mai mare numar este:", max(l))#
+#l = [i for i in range(10)]
+#print("Cel mai mare numar este:", max(l))
 
 #[METODA III]-    NU MERGE !!!!!
 I = []
