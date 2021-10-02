2a9960ceea6d465ba36dac69b33c4f420bacaf97
Resteu Mark <91153508+resteumark24@users.noreply.github.com>
2021-10-02 21:52:36 +0300

    Delete maximDinLista.py

--- maximDinLista.py
+++
@@ -1,9 +0,0 @@
-# sa se gaseasca maximul din lista
-
-l=[]
-num = int(input("How many numbers: "))
-for i in range(num):
-   number = int(input("Enter number: "))
-   l.append(number)
-print("Cel mai mare numar este:", max(l))
-
